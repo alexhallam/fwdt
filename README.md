830e7a045a1abd3de74cff0e00ace4474fef6707
alexhallam <alexhallam6.28@gmail.com>
2022-08-12 06:28:00 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#fwdt
+
+#install
+```sh
+git clone <this>
+cd fwdt
+cargo build
+```
+# example
+```sh
+./target/debug/fwdt test/data/ham_log/data.txt test/data/ham_log/template.toml 
+```
---
+++ src/main.rs
@@ -0,0 +1,345 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+// auto rerun
+// cargo install cargo-watch
+// cargo watch -c -w src -x run
+// kevin
+// fwdt
+use std::fs::read_to_string;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+use std::path::PathBuf;
+
+use csv;
+use regex::Regex;
+use std::collections::BTreeMap;
+use std::io;
+use toml::map::Map;
+use toml::Value;
+
+use structopt::StructOpt;
+#[derive(StructOpt)]
+#[structopt(
+    name = "fwdt",
+    about = "Few Word Do Trick (fwdt) is a fast logger for manual data entry that supports templates. 📝🔥\n
+    Example Usage:
+    wget data
+    wget template
+    fwdt data template
+"
+)]
+struct Cli {
+    #[structopt(name = "FILE", parse(from_os_str), help = "Data file to process")]
+    file: Option<PathBuf>,
+    #[structopt(
+        name = "TEMPLATE",
+        parse(from_os_str),
+        help = "Template file to process"
+    )]
+    template: Option<PathBuf>,
+}
+fn main() {
+    let debug: bool = false;
+
+    let opt = Cli::from_args();
+
+    // Regex helpers
+    let regex_line_comment = Regex::new("^[[:blank:]]*#").unwrap();
+    let regex_trailing_white_space = Regex::new(r#"[ \t]+$"#).unwrap();
+    let regex_blank_line = Regex::new(r#"^\s*$"#).unwrap();
+
+    // read toml file
+    let toml_file = read_to_string(&opt.template.unwrap().as_path()).unwrap();
+    let toml_parse = toml_file.parse::<Value>().unwrap();
+
+    // read data file
+    let fp: File = File::open(Path::new(&opt.file.unwrap().as_path())).unwrap();
+    let file: BufReader<&File> = BufReader::new(&fp);
+
+    // get the following from the toml file
+    //      constants
+    //      groups
+    //      observations
+    //      See: https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=e1236a33f78ac162b5887fc311a38722
+    let mut constant_keys: Vec<&str> = toml_parse["constants"]["fields"]
+        .as_array()
+        .unwrap()
+        .into_iter()
+        .map(|x| x.as_str().unwrap())
+        .collect();
+    let group_keys: Vec<_> = toml_parse["groups"].as_table().unwrap().keys().collect();
+    let group_table: &Map<String, Value> = toml_parse["groups"].as_table().unwrap();
+    //println!("{:?}", group_table);
+
+    let mut toml_obs_keys: Vec<&str> = toml_parse["obs"]["fields"]
+        .as_array()
+        .unwrap()
+        .into_iter()
+        .map(|x| x.as_str().unwrap())
+        .collect();
+    let mut obs_keys = vec!["time"];
+    obs_keys.append(&mut toml_obs_keys);
+
+    let mut date_key = vec!["date"];
+    constant_keys.append(&mut date_key);
+
+    // make empty HashMap
+    //  i
+    //  string
+    //  space_count
+    #[derive(Debug)]
+    struct DataAndConfig<'a> {
+        string: &'a str,
+        constant_keys: &'a Vec<&'a str>,
+        group_keys: &'a Vec<&'a String>,
+        group_table: &'a Map<std::string::String, Value>,
+        obs_keys: &'a Vec<&'a str>,
+        delim: &'a str,
+    }
+
+    impl DataAndConfig<'_> {
+        fn which_line_type(&self) -> LineType {
+            let string = self.string;
+            let constant_keys = self.constant_keys;
+            //let obs_keys = self.obs_keys;
+            let group_keys = self.group_keys;
+            let group_table = self.group_table;
+            let obs_keys = self.obs_keys;
+            if string.contains("date") {
+                return LineType::Date;
+            } else if is_constant_line(string, constant_keys) {
+                return LineType::Header;
+            } else if is_group_line(string, group_keys, group_table, self.delim) {
+                return LineType::Group;
+            } else if is_observation(string, obs_keys, self.delim) {
+                return LineType::Observation;
+            } else {
+                return LineType::ObservationPartial;
+            }
+        }
+
+        fn count_entries(&self) -> usize {
+            let string = self.string;
+            let vec: Vec<&str> = string.split(self.delim).collect();
+            return vec.len();
+        }
+
+        fn is_maximized(&self) -> bool {
+            let string = self.string;
+            let obs_keys: &Vec<&str> = self.obs_keys;
+            let vec: Vec<&str> = string.split(self.delim).collect();
+            return obs_keys.len() == vec.len();
+        }
+
+        fn make_vec_entries(&self) -> Vec<String> {
+            let string = self.string;
+            let vec: Vec<String> = string
+                .split(self.delim)
+                .into_iter()
+                .map(|x| x.to_owned())
+                .collect();
+            return vec;
+        }
+    } // DataAndConfig Impl
+
+    fn is_constant_line(string: &str, constant_keys: &Vec<&str>) -> bool {
+        let mut n = 0;
+        for i in 0..constant_keys.len() {
+            if string.contains(constant_keys[i]) {
+                n += 1;
+            }
+        }
+        return n == 1;
+    }
+
+    fn is_group_line(
+        string: &str,
+        group_keys: &Vec<&String>,
+        group_table: &Map<String, Value>,
+        delim: &str,
+    ) -> bool {
+        let vec_string: Vec<String> = string.split(delim).map(|x| x.to_owned()).collect();
+        let mut n = 0;
+        for i in 0..group_keys.len() {
+            let key = group_keys.clone().get(i).unwrap().to_owned();
+            let array = group_table[key].clone().try_into::<Vec<String>>().unwrap();
+            let matches = vec_string // cartisian product
+                .clone()
+                .into_iter()
+                .flat_map(|x| std::iter::repeat(x).zip(array.clone()))
+                .filter(|(a, b)| a == b)
+                .collect::<Vec<_>>();
+            if matches.len() > 0 {
+                n += 1
+            }
+        }
+        return n > 0;
+    }
+
+    fn is_observation(string: &str, obs_keys: &Vec<&str>, delim: &str) -> bool {
+        let vec_string: Vec<String> = string.split(delim).map(|x| x.to_owned()).collect();
+        return vec_string.len() == obs_keys.len();
+    }
+
+    #[derive(Debug, Clone)]
+    struct LineData {
+        num: usize,
+        string: String,
+        num_entries: usize,
+        line_type: LineType,
+        vec_entries: Vec<String>,
+        is_maximized: bool,
+    }
+
+    #[derive(Debug, Clone, Copy)]
+    enum LineType {
+        Header,
+        Group,
+        Date,
+        Observation,
+        ObservationPartial,
+    }
+
+    fn seq(stop: usize) -> Vec<usize> {
+        let mut vec = Vec::new();
+        for i in 0..stop {
+            vec.push(i);
+        }
+        return vec;
+    }
+
+    let mut numbered_file: BTreeMap<usize, LineData> = BTreeMap::new();
+    let mut btree_data_row: BTreeMap<String, String> = BTreeMap::new();
+    let mut btree_df: BTreeMap<usize, BTreeMap<String, String>> = BTreeMap::new();
+
+    if debug {
+        println!("{}", "--------------------debug--------------------");
+        println!("header keys: {:?}", constant_keys);
+        println!("group keys: {:?}", group_keys);
+        println!("group_table: {:?}", group_table);
+        println!("observation keys: {:?}", obs_keys);
+        println!("hash_map_data len: {:?}", numbered_file.len());
+        println!("constant_keys len: {:?}", constant_keys.len());
+        println!("{}", "--------------------debug--------------------");
+    }
+
+    let mut i = 0;
+    for line in file.lines() {
+        //  Clean file
+        // remove comments and empty lines
+        if regex_line_comment.is_match(line.as_ref().unwrap().as_str()) {
+            continue;
+        }
+        if regex_trailing_white_space.is_match(line.as_ref().unwrap().as_str()) {
+            continue;
+        }
+
+        if regex_blank_line.is_match(line.as_ref().unwrap().as_str()) {
+            continue;
+        }
+
+        //    let mut numbered_file: BTreeMap<usize, LineData> = BTreeMap::new();
+        let data_and_config = DataAndConfig {
+            string: line.as_ref().unwrap(),
+            constant_keys: &constant_keys,
+            group_keys: &group_keys,
+            group_table: group_table,
+            obs_keys: &obs_keys,
+            delim: " ",
+        };
+        //let line_type = which_line_type(line.as_ref().unwrap(), &constant_keys, &group_keys);
+        //let num_delim = count_entries(line.as_ref().unwrap().to_owned(), " ".to_owned());
+        let line_data = LineData {
+            num: i,
+            string: data_and_config.string.to_owned(),
+            num_entries: data_and_config.count_entries(),
+            line_type: data_and_config.which_line_type(),
+            vec_entries: data_and_config.make_vec_entries(),
+            is_maximized: data_and_config.is_maximized(),
+        };
+        if debug {
+            // println!("{:?}", line.as_ref().unwrap().to_owned());
+            // println!("{:?}", line_data.clone());
+        }
+        numbered_file.insert(i, line_data.clone());
+
+        match line_data.line_type {
+            LineType::Header => {
+                btree_data_row.insert(
+                    line_data.clone().vec_entries[0].clone(),
+                    line_data.clone().vec_entries[1].clone(),
+                );
+            }
+            LineType::Date => {
+                btree_data_row.insert("date".to_owned(), line_data.clone().vec_entries[1].clone());
+            }
+            LineType::Group => {
+                btree_data_row.insert("group".to_owned(), line_data.clone().vec_entries[1].clone());
+            }
+            LineType::Observation => {
+                for i in 0..obs_keys.len() {
+                    btree_data_row.insert(
+                        obs_keys[i].to_owned(),
+                        line_data.clone().vec_entries[i].clone(),
+                    );
+                }
+            }
+            LineType::ObservationPartial => {
+                let entries = line_data.clone().vec_entries.clone();
+                for i in 0..entries.len() {
+                    btree_data_row.insert(
+                        obs_keys[i].to_owned(),
+                        line_data.clone().vec_entries[i].clone(),
+                    );
+                }
+            }
+        };
+
+        btree_df.insert(i, btree_data_row.clone());
+        // if debug {
+        //     println!("btree_data_row: {:?}", btree_data_row.clone());
+        //     println!("btree_df: {:?}", btree_df.clone());
+        // }
+
+        i += 1;
+    }
+
+    // the node entry is the most complete entry and should be the first
+    // of the observations
+    let mut complete_observation_entries: BTreeMap<usize, LineData> = numbered_file.clone();
+    complete_observation_entries.retain(|_, v| v.is_maximized == true);
+    let obs_node_key: &usize = complete_observation_entries.iter().next().unwrap().0;
+
+    // make a BtreeMap that holds the data. The keys are the column names. The values are Some(entries). Matching can be done to see if empty.
+    // group is
+
+    //filter rows where btree_df is greater than obs_node_key
+
+    btree_df.retain(|k, _| k >= obs_node_key);
+
+    let mut btree_df_values: BTreeMap<usize, Vec<&String>> = BTreeMap::new();
+
+    for i in *obs_node_key..btree_df.len() {
+        let vec_string = btree_df.get(&i).unwrap().values().collect::<Vec<&String>>();
+        btree_df_values.insert(i - *obs_node_key, vec_string);
+    }
+
+    let mut wtr = csv::Writer::from_writer(io::stdout());
+
+    let col_names: Vec<&String> = btree_data_row.keys().collect();
+
+    wtr.write_record(col_names.clone());
+
+    for i in 0..btree_df_values.len() {
+        wtr.write_record(btree_df_values.get(&i).unwrap());
+        //println!("{:?}", btree_df_values.get(&i).unwrap());
+    }
+
+    if debug {
+        println!("{}", "--------------------debug--------------------");
+        //println!("btree_df_values: {:#?}", btree_df_values.clone());
+        println!("btree_data_row: {:#?}", btree_data_row.clone().keys());
+        println!("{}", "--------------------debug--------------------");
+    }
+}
